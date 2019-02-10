8710576ae75da5c3a7d3c47b489f91c82403bdf4
Jerry Wu <wuhaoyu@bu.edu>
2019-02-10 11:36:23 -0500

    Implement search functionality
    Signed-off-by: Jerry Wu <wuhaoyu@bu.edu>

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,30 +1,34 @@
 import React from 'react'
-import {ShotChart} from "./ShortChart"
 import {DataViewContainer} from "./DataViewContainer"
 import {Profile} from "./Profile"
+import {SearchBar} from "./SearchBar"
+import {DEFAULT_PLAYER_INFO} from "../constants"
 import nba from 'nba'
 
 export class Main extends React.Component {
 	state = {
-		playerInfo: {
-			playerId: nba.findPlayer('Stephen Curry').playerId,
-			teamAbbreviation: 'GSW',
-		}
+		playerInfo: DEFAULT_PLAYER_INFO,
 	}
 
 	componentDidMount() {
-		nba.stats.playerInfo({PlayerID: nba.findPlayer('Stephen Curry').playerId}).then((info) => {
+		this.loadPlayerInfo(this.state.playerInfo.fullName);
+	}
+
+	loadPlayerInfo = (playerName) => {
+		nba.stats.playerInfo({ PlayerID: nba.findPlayer(playerName).playerId }).then((info) => {
 			const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
-			console.log(playerInfo);
-			this.setState({playerInfo});
+			this.setState({ playerInfo });
 		});
 	}
 
 	render() {
 		return (
 			<div className="main">
-				<Profile playerInfo={this.state.playerInfo}/>
-				<DataViewContainer playerId={this.state.playerInfo.playerId}/>
+				<SearchBar loadPlayerInfo={this.loadPlayerInfo}/>
+				<div className="player">
+					<Profile playerInfo={this.state.playerInfo}/>
+					<DataViewContainer playerId={this.state.playerInfo.playerId}/>
+				</div>
 			</div>
 		);
 	}
